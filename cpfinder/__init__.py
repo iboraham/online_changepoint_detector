bd28df2ea2314e4b16e51615f7dd9d2b799da598
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-07-29 12:57:47 +0100

    Update __init__.py

--- cpfinder/__init__.py
+++ cpfinder/__init__.py
@@ -5,9 +5,7 @@ import pandas as pd
 
 
 class cpfinder:
-    def __init__(
-        self, data: pd.DataFrame, method: str, channel_id: str, interval: int = 100
-    ):
+    def __init__(self, data: pd.DataFrame, method: str):
         """
         Finds the changepoints with various methods.
 
@@ -19,24 +17,11 @@ class cpfinder:
         """
         self.method = method
         self.data = data
-        self.channel_id = channel_id
-        self.interval = interval
 
     def fit(self):
-        # Data prep.
-        self.data, self.dt = clean_data(self.data, self.channel_id)
-
-        # Divide the P,PF,E data
-        power = self.data[:, 0]
-        power_factor = self.data[:, 1]
-        energy = self.data[:, 2]
 
         # Given method to function
         method = str_to_func(self.method)
 
-        # Power
         detector = method("auto")
-        cps_p = detector.fit(power, self.interval)
-        cps_pf = detector.fit(power_factor, self.interval)
-        cps_e = detector.fit(energy, self.interval)
-        return cps_p, cps_pf, cps_e
+        return detector.fit(self.data)
