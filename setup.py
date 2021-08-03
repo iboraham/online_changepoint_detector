bedc041bfc06797205fab9cf746fea0521f5fe31
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-08-03 14:29:29 +0100

    setup files
    pip installl files

---
+++ setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+
+setup(
+    name="cpfinder",
+    packages=["cpfinder"],
+    version="0.1",
+    license="MIT",
+    description="Online changepoint detection for python",
+    author="Ibrahim Onur Serbetci",
+    author_email="ionurserbetci@gmail.com",
+    url="https://github.com/iboraham/online_changepoint_detector",
+    download_url="https://github.com/iboraham/online_changepoint_detector/archive/v_01.tar.gz",
+    keywords=[
+        "online changepoint detection",
+        "changepoint detection",
+        "rulsif",
+        "bayesian",
+    ],
+    install_requires=[
+        "scipy",
+        "pandas",
+        "sklearn",
+        "numpy",
+        "matplotlib",
+        "tqdm",
+        "roerich",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
