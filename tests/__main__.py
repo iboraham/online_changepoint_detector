42812b9da4e3c1f3bbb1e6ef761f6d40346d0130
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-08-06 03:59:50 +0100

    tests update

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,13 +1,5 @@
-import os, sys, inspect
-
-
-def _add_parent_folder():
-    currentdir = os.path.dirname(
-        os.path.abspath(inspect.getfile(inspect.currentframe()))
-    )
-    parentdir = os.path.dirname(currentdir)
-    sys.path.insert(0, parentdir)
-
+import test_datasets
+from utils import *
 
 _add_parent_folder()
 
@@ -18,7 +10,4 @@ from cpfinder.datasets import generate_normal_time_series
 
 
 if __name__ == "__main__":
-    data = generate_normal_time_series(3)
-    detector = cpfinder(data=data, method="rulsif")
-    detector.fit(animationFlag=True, interval=100, plotFlag=True)
-    # detector.saveAnimationVideo("assets/animation.gif")
+    test_datasets()
---
+++ tests/utils.py
@@ -0,0 +1,9 @@
+import os, sys, inspect
+
+
+def _add_parent_folder():
+    currentdir = os.path.dirname(
+        os.path.abspath(inspect.getfile(inspect.currentframe()))
+    )
+    parentdir = os.path.dirname(currentdir)
+    sys.path.insert(0, parentdir)
