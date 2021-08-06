c9546adea2259b0e357c036aeca2bf1fd34003a4
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-08-06 01:02:08 +0100

    rulsif plot update

--- cpfinder/vis/plot_mtp.py
+++ cpfinder/vis/plot_mtp.py
@@ -1,8 +1,6 @@
-import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LogNorm
-import matplotlib.dates as mdates
 
 
 def plot_posterior(
@@ -92,3 +90,44 @@ def plot_posterior_animation(data, fig, axes, dt, R, pmean, pvar, cps, title=Non
         )
     ]
     fig.legend(handles, labels)
+
+
+def display(X, n, T, L, S, Ts, peaks=None, plot_peak_height=10, s_max=10):
+
+    plt.cla()
+    for i in range(X.shape[1]):
+
+        plt.subplot(n, 1, i + 1)
+        ax = X[:, i]
+        plt.plot(T, ax, linewidth=2, label="Original signal", color="C0")
+        for t in T[L == 1]:
+            plt.plot([t] * 2, [ax.min(), ax.max()], color="0", linestyle="--")
+        plt.ylim(ax.min(), ax.max())
+        plt.xlim(0, T.max())
+        plt.xticks(size=16)
+        plt.yticks(size=16)
+
+    score_plot_ix = n if peaks is None else n - 1
+    plt.subplot(n, 1, score_plot_ix)
+    plt.cla()
+    plt.plot(Ts, S, linewidth=3, label="Change-point score", color="C3")
+    plt.xlim(0, T.max())
+    for t in T[L == 1]:
+        plt.plot([t] * 2, [-1, s_max], color="0", linestyle="--")
+
+    # display find peaks #todo refactoring
+    if peaks is not None:
+        plt.subplot(n, 1, n)
+        new_score_peaks = np.zeros(len(T))
+        new_score_peaks[peaks] = plot_peak_height
+        plt.plot(new_score_peaks, linewidth=3, label="Peaks", color="C4")
+        for t in T[L == 1]:
+            plt.plot([t] * 2, [-1, s_max], color="0", linestyle="--")
+
+    plt.ylim(-1, s_max)
+    plt.xlim(0, T.max())
+    plt.xticks(size=16)
+    plt.yticks(np.arange(0, s_max + 1, 5), size=16)
+    plt.xlabel("Time", size=16)
+    plt.legend(fontsize=16)
+    plt.tight_layout()
