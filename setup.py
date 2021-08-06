d84bf0193e6e9d51c604cd4273e514844611c513
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-08-06 12:17:35 +0100

    setuo

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name="cpfinder",
@@ -11,9 +18,11 @@ setup(
         "cpfinder.utils",
         "cpfinder.vis",
     ],
-    version="0.2",
+    version="0.2.1",
     license="MIT",
     description="Online changepoint detection for python",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="Ibrahim Onur Serbetci",
     author_email="ionurserbetci@gmail.com",
     url="https://github.com/iboraham/online_changepoint_detector",
