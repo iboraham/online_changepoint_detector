22f4d012a75a68da4ce5689145ddbe6a4ef64b5c
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-08-06 01:13:57 +0100

    Rulsif update: Add find_peaks

--- cpfinder/utils/__init__.py
+++ cpfinder/utils/__init__.py
@@ -1,5 +1,6 @@
-from cpfinder.methods import bocpd
-from cpfinder.methods import rulsif
+import numpy as np
+import pandas as pd
+from scipy.signal import find_peaks as fp
 
 
 def str_to_func(string: str):
@@ -12,7 +13,18 @@ def str_to_func(string: str):
     Returns:
         func: method
     """
+    from cpfinder.methods import bocpd
+    from cpfinder.methods import rulsif
+
     if string == "bocpd":
         return bocpd
     if string == "rulsif":
         return rulsif
+
+
+def smoothen(x, winsize=5):
+    return np.array(pd.Series(x).rolling(winsize).mean())[winsize - 1 :]
+
+
+def find_peaks(score):
+    return fp(score, height=1, distance=50)[0]
