2f656379fc426a31eadf05b8981171371c665b1b
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-08-06 01:17:29 +0100

    Update online_rulsif.py

--- cpfinder/methods/online_rulsif.py
+++ cpfinder/methods/online_rulsif.py
@@ -6,44 +6,6 @@ from cpfinder.vis import roerich_display_edited
 from cpfinder.utils import find_peaks
 
 
-def online_changepoint_detector(X):
-    """
-    Creates animatation of the Generalization of Change-Point Detection
-    in Time Series Data Based on Direct Density Ratio Estimation by
-    matplotlib.animation package.
-
-    Org. Paper:
-        https://arxiv.org/abs/2001.06386
-        https://github.com/HSE-LAMBDA/roerich
-
-    Args:
-        X (Array-like): Time series values that changepoint detector will work on
-
-    Returns:
-        array-like: score of the change point
-        array-like: peak points
-    """
-    T = np.arange(len(X))
-
-    cpd = roerich.OnlineNNRuLSIF(
-        net="default",
-        scaler="default",
-        metric="KL_sym",
-        periods=1,
-        window_size=10,
-        lag_size=500,
-        step=10,
-        n_epochs=10,
-        lr=0.1,
-        lam=0.0001,
-        optimizer="Adam",
-        alpha=0.1,
-    )
-
-    score, peaks = cpd.predict(X)
-    return score, peaks
-
-
 def animate_rulsif(i, *args):
     data, L, cpd = args
     X = data[:i]
