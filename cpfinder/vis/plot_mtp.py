e7a0247b4f61b79e23be4ec361ee1d40c5bb7547
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-08-16 23:25:40 +0100

    Update plot_mtp.py

--- cpfinder/vis/plot_mtp.py
+++ cpfinder/vis/plot_mtp.py
@@ -7,7 +7,7 @@ def plot_posterior(
     data, dt, R, pmean, pvar, cps, title=None, save=False, save_name=None
 ):
     T = len(data)
-    fig, axes = plt.subplots(2, 1, figsize=(20, 8))
+    fig, axes = plt.subplots(2, 1, figsize=(15, 10))
     _plot(data, fig, axes, dt, R, pmean, pvar, cps, title, T)
     if save:
         plt.savefig(save_name)
@@ -50,16 +50,24 @@ def _extras(fig, cps, ax1, ax2):
 
 def _ax2_plot(R, T, ax2):
     ax2.imshow(
-        np.rot90(R), aspect="auto", cmap="gray_r", norm=LogNorm(vmin=0.0001, vmax=1)
+        np.flipud(np.rot90(R)),
+        aspect="auto",
+        cmap="gray_r",
+        norm=LogNorm(vmin=0.0001, vmax=1),
+        origin="lower",
     )
+    ax2.set_xlabel("Time Index", fontsize=20)
+    ax2.set_ylabel("Run Length", fontsize=20)
     maxes = R.argmax(axis=1)
-    ax2.plot(len(R) - maxes, c="r", linewidth=3, label="Maxes")
+    ax2.plot(maxes, c="r", linewidth=3, label="Maxes")
     ax2.set_xlim([0, T])
     ax2.margins(0)
 
 
 def _ax1_plot(data, dt, pmean, pvar, ax1):
     ax1.scatter(dt, data)
+    ax1.set_xlabel("Date", fontsize=20)
+    ax1.set_ylabel("Value", fontsize=20)
     ax1.plot(dt, data)
     ax1.set_xlim([dt[0], dt[-1]])
     ax1.margins(0)
